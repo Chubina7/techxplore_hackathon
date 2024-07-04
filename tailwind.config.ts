552588683ed0d705f7e1d599457ab3659c793fb4
Luka Chubinidze <chubinidzel08@gmail.com>
2024-07-04 10:45:03 +0400

    initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        customWhite: "#D9D9D9",
+        customPurpleLight: "#9D85F2",
+        customPurpleDark: "#",
       },
-    },
+    }
   },
   plugins: [],
 };
