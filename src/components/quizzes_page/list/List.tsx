44b2211174607c95aa23854d17b65f48da9e5353
Luka Chubinidze <chubinidzel08@gmail.com>
2024-07-04 22:54:02 +0400

    change conditionally what to show on responsive of homepage

--- src/components/quizzes_page/list/List.tsx
+++ src/components/quizzes_page/list/List.tsx
@@ -3,7 +3,7 @@ import Item from "../../ui/horizontal_list/list/item/Item";
 
 export default function List() {
   return (
-    <div className="w-full grid grid-cols-4 gap-5">
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5">
       {dummydata.map((quizz) => (
         <Item key={quizz.id} data={quizz} />
       ))}
